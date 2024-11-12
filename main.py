3e0f6c6a0d983f91151e1272f09547e9fe847f07
InGyu <yangik11@naver.com>
2024-11-12 16:14:24 +0900

    동적 분석 기능 추가

--- main.py
+++ main.py
@@ -1,130 +1,143 @@
 from http.client import responses
-from imghdr import tests
-from linecache import cache
-from xml.sax.saxutils import escape
 
 import requests
 import fire
 import json
 
-from Tools.scripts.eptags import treat_file
 from requests_toolbelt import MultipartEncoder
 
-SERVER = "http://127.0.0.1:8000"
+SERVER = ''
 APPNAME = ''
 API_KEY = ''
+ADB_IDENTIFIER = ''
 HASH = ''
+DATA_HASH = ''
+API_KEY_HEADERS = None
 
-def start(appname, api_key):
-    global APPNAME, API_KEY
+
+def start(server, appname, api_key, identifier):
+    global SERVER, APPNAME, API_KEY, ADB_IDENTIFIER, API_KEY_HEADERS, DATA_HASH
+    SERVER = server
     APPNAME = appname
     API_KEY = api_key
+    ADB_IDENTIFIER = {'identifier': identifier}
+    API_KEY_HEADERS = {'Authorization': api_key}
     data = upload()
     print("Start Static Analysis")
     scan(data)
-    pdf()
-    json_report()
+    static_pdf()
+    static_json()
     print("Start Dynamic Analysis")
+    mobsfy()
     start_dynamic_analysis()
     set_proxy()
+    test_activity("exported")
+   # test_activity("activity")
+    tls_test()
+    dynamic_json()
 
 def upload():
+    global HASH, DATA_HASH
     print(f"upload {APPNAME}")
-    try :
-        multipart_data = MultipartEncoder(fields={'file': (APPNAME, open(APPNAME, 'rb'), 'application/octet-stream')})
-        headers = {'Content-Type': multipart_data.content_type, 'Authorization': API_KEY}
-        response = requests.post(SERVER + '/api/v1/upload', data=multipart_data, headers=headers)
-        if response.status_code != 200:
-            print(f"failed to upload {APPNAME} : {response.content}")
-            exit(1)
-        global HASH
-        HASH = response.json()['hash']
-        print(f"Successfully Upload File : APK : {APPNAME}, Hash : {HASH}")
-        return response.text
-    except Exception as e:
-        print("ERROR " + str(e))
+    multipart_data = MultipartEncoder(fields={'file': (APPNAME, open(APPNAME, 'rb'), 'application/octet-stream')})
+    headers = {'Content-Type': multipart_data.content_type, 'Authorization': API_KEY}
+    response = requests.post(SERVER + '/api/v1/upload', data=multipart_data, headers=headers)
+    if response.status_code != 200:
+        print(f"failed to upload {APPNAME} : {response.content}")
+        exit(1)
+    HASH = response.json()['hash']
+    DATA_HASH = {"hash": HASH}
+    print(f"Successfully Upload File : APK : {APPNAME}, Hash : {HASH}")
+    return response.text
+
 
 def scan(data):
+    global DATA_HASH
     print("Scanning APK Hash : " + HASH)
-    headers = {'Authorization': API_KEY}
-    try:
-        APK_info = json.loads(data)
-        response = requests.post(SERVER + '/api/v1/scan', data=APK_info, headers=headers)
-        if response.status_code != 200:
-            print(f"failed to scan {APPNAME} : {response.content}")
-            exit(1)
-        print(f"Successfully scan File")
-    except Exception as e :
-        print("ERROR " + str(e))
+    apk_info = json.loads(data)
+    response = requests.post(SERVER + '/api/v1/scan', data=apk_info, headers=API_KEY_HEADERS)
+    if response.status_code != 200:
+        print(f"failed to scan {APPNAME} : {response.content}")
         exit(1)
+    print(f"Successfully scan File")
 
 
-def pdf():
+def static_pdf():
     print("Generate static analysis report to PDF")
-    headers = {'Authorization': API_KEY}
-    data = {"hash": HASH}
-    try :
-        response = requests.post(SERVER + '/api/v1/download_pdf', data=data, headers=headers, stream=True)
-        if response.status_code != 200:
-            print(f"failed generating static analysis report pdf : {response.content}")
-            exit(1)
-        with open(f"{APPNAME}_{HASH}_static.pdf", "wb") as f:
-            for chunk in response.iter_content(chunk_size=1024):
-                if chunk:
-                    f.write(chunk)
-        print(f"Static PDF saved as {APPNAME}_{HASH}_static.pdf")
-    except Exception as e :
-        print("ERROR " + str(e))
+    response = requests.post(SERVER + '/api/v1/download_pdf', data=DATA_HASH, headers=API_KEY_HEADERS, stream=True)
+    if response.status_code != 200:
+        print(f"failed generating static analysis report pdf : {response.content}")
         exit(1)
+    with open(f"{APPNAME}_{HASH}_static.pdf", "wb") as f:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+    print(f"Static PDF saved as {APPNAME}_{HASH}_static.pdf")
 
-def json_report():
+
+def static_json():
     print("Generate static analysis report to JSON")
-    headers = {'Authorization': API_KEY}
-    data = {"hash": HASH}
-    try :
-        response = requests.post(SERVER + '/api/v1/report_json', data=data, headers=headers, stream=True)
-        if response.status_code != 200:
-            print(f"failed generating static analysis report json : {response.content}")
-            exit(1)
-        with open(f"{APPNAME}_{HASH}_static.json", "wb") as f:
-            f.write(response.content)
-        print(f"Static json saved as {APPNAME}_{HASH}_static.json")
-    except Exception as e :
-        print("ERROR " + str(e))
+    response = requests.post(SERVER + '/api/v1/report_json', data=DATA_HASH, headers=API_KEY_HEADERS, stream=True)
+    if response.status_code != 200:
+        print(f"failed generating static analysis report json : {response.content}")
         exit(1)
+    with open(f"{APPNAME}_{HASH}_static.json", "wb") as f:
+        f.write(response.content)
+    print(f"Static json saved as {APPNAME}_{HASH}_static.json")
+
 
 def start_dynamic_analysis():
-    headers = {'Authorization': API_KEY}
-    data = {"hash": HASH}
-    try :
-        response = requests.post(SERVER + '/api/v1/dynamic/start_analysis', data=data, headers=headers)
-        if response.status_code != 200:
-            print(f"failed to start analysis : {response.content}")
-            exit(1)
-        print(f"Successfully Start Dynamic Analysis")
-    except Exception as e :
-        print("ERROR " + str(e))
+    response = requests.post(SERVER + '/api/v1/dynamic/start_analysis', data=DATA_HASH, headers=API_KEY_HEADERS)
+    if response.status_code != 200:
+        print(f"failed to start analysis : {response.content}")
+        exit(1)
+    print(f"Successfully Start Dynamic Analysis")
+
 
 def set_proxy():
-    headers = {'Authorization': API_KEY}
-    set = {'action': 'set'}
-    try :
-        response = requests.post(SERVER + '/api/v1/android/global_proxy', data=set, headers=headers)
-        if response.status_code != 200:
-            print(f"failed to set proxy : {response.content}")
-            exit(1)
-        print(f"Successfully Set Proxy")
-    except Exception as e :
-        print("ERROR " + str(e))
+    print("Set Proxy")
+    _set = {'action': 'set'}
+    response = requests.post(SERVER + '/api/v1/android/global_proxy', data=_set, headers=API_KEY_HEADERS)
+    if response.status_code != 200:
+        print(f"failed to set proxy : {response.content}")
+        exit(1)
+    print(f"Successfully Set Proxy")
 
-def active_activity():
-    try :
-        headers = {'Authorization': API_KEY}
-        test = {'test' : }
+def mobsfy():
+    print("MobSFY android runtime environment")
+    response = requests.post(SERVER + '/api/v1/android/mobsfy', data=ADB_IDENTIFIER,headers=API_KEY_HEADERS)
+    if response.status_code != 200:
+        print(f"failed to mobsfy : {response.content}")
+        exit(1)
+    print("Successfully MobSfy")
+
+def test_activity(action):
+    print(f"Start test {action} Activity")
+    data = {"hash": HASH, "test": action}
+    response = requests.post(SERVER + '/api/v1/android/activity', data=data, headers=API_KEY_HEADERS)
+    if response.status_code != 200:
+        print(f"failed to test activity : {response.content}")
+        exit(1)
+    print(f"Successfully tested {action} Activity")
+
+def tls_test():
+    print("Start TLS/SSL Test")
+    response = requests.post(SERVER + '/api/v1/android/tls_tests', data=DATA_HASH, headers=API_KEY_HEADERS)
+    if response.status_code != 200:
+        print(f"failed to tls_test : {response.content}")
+        exit(1)
+    print("Successfully TLS/SSL Test")
+
+def dynamic_json():
+    print("Generate dynamic analysis report to JSON")
+    response = requests.post(SERVER + '/api/v1/dynamic/report_json', data=DATA_HASH, headers=API_KEY_HEADERS)
+    if response.status_code != 200:
+        print(f"failed to generate dynamic analysis report json : {response.content}")
+        exit(1)
+    with open(f"{APPNAME}_{HASH}_dynamic.json", "wb") as f:
+        f.write(response.content)
+    print(f"Dynamic json saved as {APPNAME}_{HASH}_dynamic.json")
 
 
-
-
-
-if __name__ == "__main__" :
+if __name__ == "__main__":
     fire.Fire(start)
