1d537f7ba2bdfc974361652d9b88e84ea1be19a5
InGyu <yangik11@naver.com>
2024-11-13 15:47:24 +0900

    로그 한국어로 바꿈

--- main.py
+++ main.py
@@ -24,132 +24,143 @@ def start(server, apppath, api_key, identifier):
     API_KEY = api_key
     ADB_IDENTIFIER = {'identifier': identifier}
     API_KEY_HEADERS = {'Authorization': api_key}
+    print("복호화 시작")
     decrypt = decrypt_repack
     APPNAME = decrypt.decrypt_and_repack(APPPATH, "dbcdcfghijklmaop")
-    print(APPNAME)
+    print(f"복호화 완료 APK{APPNAME}")
     data = upload()
-    print("Start Static Analysis")
+    print("정적 분석 시작")
     scan(data)
     static_pdf()
     static_json()
-    print("Start Dynamic Analysis")
+    print("동적 분석 시작")
     mobsfy()
     start_dynamic_analysis()
     set_proxy()
     test_activity("exported")
     test_activity("activity")
     tls_test()
+    dynamic_stop()
     dynamic_json()
 
 
 def upload():
     global HASH, DATA_HASH
-    print(f"upload {APPNAME}")
+    print(f"{APPNAME} 업로드 시작")
     multipart_data = MultipartEncoder(fields={'file': (APPNAME, open(APPNAME, 'rb'), 'application/octet-stream')})
     headers = {'Content-Type': multipart_data.content_type, 'Authorization': API_KEY}
     response = requests.post(SERVER + '/api/v1/upload', data=multipart_data, headers=headers)
     if response.status_code != 200:
-        print(f"failed to upload {APPNAME} : {response.content}")
+        print(f"{APPNAME} : {response.content}")
         exit(1)
     HASH = response.json()['hash']
     DATA_HASH = {"hash": HASH}
-    print(f"Successfully Upload File : APK : {APPNAME}, Hash : {HASH}")
+    print(f"업로드 됨. APK : {APPNAME}, Hash : {HASH}")
     return response.text
 
 
 def scan(data):
     global DATA_HASH
-    print("Scanning APK Hash : " + HASH)
+    print("앱 스캔 시작 Hash : " + HASH)
     apk_info = json.loads(data)
     response = requests.post(SERVER + '/api/v1/scan', data=apk_info, headers=API_KEY_HEADERS)
     if response.status_code != 200:
-        print(f"failed to scan {APPNAME} : {response.content}")
+        print(f"스캔 실패 {APPNAME} : {response.content}")
         exit(1)
-    print(f"Successfully scan File")
+    print("스캔 완료함.")
 
 
 def static_pdf():
-    print("Generate static analysis report to PDF")
+    print("정적 분석 결과를 PDF 파일로 생성 중")
     response = requests.post(SERVER + '/api/v1/download_pdf', data=DATA_HASH, headers=API_KEY_HEADERS, stream=True)
     if response.status_code != 200:
-        print(f"failed generating static analysis report pdf : {response.content}")
+        print(f"파일 생성 실패 : {response.content}")
         exit(1)
     with open(f"{APPNAME}_{HASH}_static.pdf", "wb") as f:
         for chunk in response.iter_content(chunk_size=1024):
             if chunk:
                 f.write(chunk)
-    print(f"Static PDF saved as {APPNAME}_{HASH}_static.pdf")
+    print(f"{APPNAME}_{HASH}_static.pdf로 파일이 생성됨.")
+
 
 
 def static_json():
-    print("Generate static analysis report to JSON")
+    print("정적 분석 결과를 JSON 파일로 생성 중")
     response = requests.post(SERVER + '/api/v1/report_json', data=DATA_HASH, headers=API_KEY_HEADERS, stream=True)
     if response.status_code != 200:
-        print(f"failed generating static analysis report json : {response.content}")
+        print(f"파일 생성 실패 : {response.content}")
         exit(1)
     with open(f"{APPNAME}_{HASH}_static.json", "wb") as f:
         f.write(response.content)
-    print(f"Static json saved as {APPNAME}_{HASH}_static.json")
+    print(f"{APPNAME}_{HASH}_static.json로 파일이 생성됨")
 
 
 def start_dynamic_analysis():
-    print("Processing")
+    print("동적 분석 시작 하는중 ...")
     response = requests.post(SERVER + '/api/v1/dynamic/start_analysis', data=DATA_HASH, headers=API_KEY_HEADERS)
     if response.status_code != 200:
-        print(f"failed to start analysis : {response.content}")
+        print(f"동적 분석 실행 실패 : {response.content}")
         exit(1)
-    print(f"Successfully Start Dynamic Analysis")
+    print(f"동적 분석 시작 성공")
 
 
 def set_proxy():
-    print("Set Proxy")
+    print("프록시 설정 중")
     _set = {'action': 'set'}
     response = requests.post(SERVER + '/api/v1/android/global_proxy', data=_set, headers=API_KEY_HEADERS)
     if response.status_code != 200:
-        print(f"failed to set proxy : {response.content}")
+        print(f"프록시 설정 실패 : {response.content}")
         exit(1)
-    print(f"Successfully Set Proxy")
+    print(f"프록시 설정 완료")
 
 def mobsfy():
-    print("MobSFY android runtime environment set")
+    print("안드로이드 환경 구축 중 ...")
     response = requests.post(SERVER + '/api/v1/android/mobsfy', data=ADB_IDENTIFIER,headers=API_KEY_HEADERS)
     if response.status_code != 200:
-        print(f"failed to mobsfy : {response.content}")
+        print(f"환경 구축 실패 : {response.content}")
         exit(1)
-    print("Successfully MobSfy")
+    print("환경 구축 성공")
 
 def test_activity(action):
     if action == "activity":
-        print("Start Activity")
+        print("Activity 시작")
     else:
-        print(f"Start {action} Activity")
+        print(f"{action} Activity 테스트 시작")
     data = {"hash": HASH, "test": action}
     response = requests.post(SERVER + '/api/v1/android/activity', data=data, headers=API_KEY_HEADERS)
     if response.status_code != 200:
-        print(f"failed to test activity : {response.content}")
+        print(f"Activity 테스트 실패 : {response.content}")
         exit(1)
     if action == "activity":
-        print("Successfully tested Activity")
+        print("Activity 테스트 성공")
     else:
-        print(f"Successfully tested {action} Activity")
+        print(f"{action} Activity 테스트 성공")
 
 def tls_test():
-    print("Start TLS/SSL Test")
+    print("TLS/SSL 테스트 시작")
     response = requests.post(SERVER + '/api/v1/android/tls_tests', data=DATA_HASH, headers=API_KEY_HEADERS)
     if response.status_code != 200:
-        print(f"failed to tls_test : {response.content}")
+        print(f"TLS/SSL 테스트 실패 : {response.content}")
         exit(1)
-    print("Successfully TLS/SSL Test")
+    print("TLS/SSL 성공")
+
+def dynamic_stop():
+    print("동적 분석 정지 중...")
+    response = requests.post(SERVER + '/api/v1/dynamic/stop_analysis', data=DATA_HASH, headers=API_KEY_HEADERS)
+    if response.status_code != 200:
+        print(f"동적 분석 정지 실패 : {response.content}")
+        exit(1)
+    print("동적 분석 정지 완료")
 
 def dynamic_json():
-    print("Generate dynamic analysis report to JSON")
+    print("동적 분석 결과를 JSON 파일로 생성 중...")
     response = requests.post(SERVER + '/api/v1/dynamic/report_json', data=DATA_HASH, headers=API_KEY_HEADERS)
     if response.status_code != 200:
-        print(f"failed to generate dynamic analysis report json : {response.content}")
+        print(f"파일 생성 실패 : {response.content}")
         exit(1)
     with open(f"{APPNAME}_{HASH}_dynamic.json", "wb") as f:
         f.write(response.content)
-    print(f"Dynamic json saved as {APPNAME}_{HASH}_dynamic.json")
+    print(f"{APPNAME}_{HASH}_dynamic.json로 파일이 생성됨")
 
 
 if __name__ == "__main__":
